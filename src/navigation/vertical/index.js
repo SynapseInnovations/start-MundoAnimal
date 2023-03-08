bf9032d71267a92a9dc93c9f0767304fb9e1a898
Zeretto4210 <Zeretto4210@live.com>
2023-03-08 20:06:40 -0300

    Enrutado completo

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,45 +1,100 @@
 const navigation = () => {
   return [
     {
+      sectionTitle: 'Menú Principal'
+    },
+    {
       title: 'Home',
       path: '/home',
       icon: 'mdi:home-outline'
     },
     {
-      title: 'Second Page',
-      path: '/second-page',
-      icon: 'mdi:email-outline'
+      sectionTitle: 'Panel de Operador'
     },
     {
       title: 'Inventario',
-      path: '/mui',
-      icon: 'mdi:application'
-    },
-    {
-      title: 'Mantenedor',
-      path: '/',
-      icon: 'mdi:application'
-    },
-    {
-      title: 'Checkout',
-      path: '/checkout'
+      icon: 'mdi:database',
+      children: [
+        {
+          title: 'Productos',
+          path: '/operator/inventory/products',
+          icon: 'mdi:archive'
+        },
+        {
+          title: 'Marcas',
+          path: '/operator/inventory/brands',
+          icon: 'mdi:briefcase'
+        },
+        {
+          title: 'Categorías',
+          path: '/operator/inventory/categories',
+          icon: 'mdi:shape'
+        },
+        {
+          title: 'Animales',
+          path: '/operator/inventory/pets',
+          icon: 'mdi:paw'
+        }
+      ]
     },
     {
       title: 'Ventas',
       path: '/',
-      icon: 'mdi:application'
+      icon: 'mdi:cart',
+      children: [
+        {
+          title: 'Generar Venta',
+          path: '/operator/sales/generate',
+          icon: 'mdi:cart'
+        },
+        {
+          title: 'Listar Ventas',
+          path: '/operator/sales/list',
+          icon: 'mdi:list-box'
+        }
+      ]
+    },
+    {
+      sectionTitle: 'Panel de Administrador'
     },
     {
       title: 'Usuarios',
       path: '/',
-      icon: 'mdi:application'
+      icon: 'mdi:account-multiple',
+      children: [
+        {
+          title: 'Crear Usuarios',
+          path: '/admin/users/create',
+          icon: 'mdi:account-multiple-plus'
+        },
+        {
+          title: 'Administrar Cuentas',
+          path: '/admin/users/manage',
+          icon: 'mdi:account-box-multiple'
+        }
+      ]
     },
     {
-      path: '/acl',
-      action: 'read',
-      subject: 'acl-page',
-      title: 'Access Control',
-      icon: 'mdi:shield-outline'
+      title: 'Finanzas',
+      path: '/',
+      icon: 'mdi:cash-multiple',
+      children: [
+        {
+          title: 'Ventas del día',
+          path: '/admin/finance/daily',
+          icon: 'mdi:cash-register'
+        },
+        {
+          title: 'Ventas por Periodo',
+          path: '/admin/finance/history',
+          icon: 'mdi:cash-clock'
+        },
+        {
+          title: 'Ganancias',
+          path: '/admin/finance/earnings',
+          icon: 'mdi:cash-plus'
+        }
+      ]
     }
   ]
 }

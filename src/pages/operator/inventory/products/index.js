d3dbc9894e47947521e075681cb7ca751680831d
erojo1 <114534409+erojo1@users.noreply.github.com>
2023-03-20 22:38:58 -0300

    Inventario

--- src/pages/operator/inventory/products/index.js
+++ src/pages/operator/inventory/products/index.js
@@ -1,31 +1,219 @@
+// ** React Imports
+import { useState, useEffect, useCallback } from 'react'
+import { Provider } from 'react-redux'
+
 // ** MUI Imports
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
+import Alert from '@mui/material/Alert'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import { DataGrid } from '@mui/x-data-grid'
+import Checkbox from '@mui/material/Checkbox'
+import FormGroup from '@mui/material/FormGroup'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
+import AlertTitle from '@mui/material/AlertTitle'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import FormControlLabel from '@mui/material/FormControlLabel'
 
-const ProductsIndex = () => {
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Store Imports
+import { useDispatch, useSelector } from 'react-redux'
+
+// ** Custom Components Imports
+import CustomChip from 'src/@core/components/mui/chip'
+import PageHeader from 'src/@core/components/page-header'
+import TableHeader from 'src/views/apps/permissions/TableHeader'
+
+// ** Actions Imports
+import { fetchData } from 'src/store/apps/permissions'
+
+const colors = {
+  support: 'info',
+  users: 'success',
+  manager: 'warning',
+  administrator: 'primary',
+  'restricted-user': 'error'
+}
+
+const defaultColumns = [
+  {
+    flex: 0.2,
+    field: 'name',
+    minWidth: 240,
+    headerName: 'Nombre del Producto',
+    renderCell: ({ row }) => <Typography>{row.name}</Typography>
+  },
+  {
+    flex: 0.35,
+    minWidth: 90,
+    field: 'assignedTo',
+    headerName: 'Cantidad',
+    renderCell: ({ row }) => {
+      return row.assignedTo.map((assignee, index) => (
+        <CustomChip
+          size='small'
+          key={index}
+          skin='light'
+          color={colors[assignee]}
+          label={assignee.replace('-', ' ')}
+          sx={{ '& .MuiChip-label': { textTransform: 'capitalize' }, '&:not(:last-of-type)': { mr: 3 } }}
+        />
+      ))
+    }
+  },
+  {
+    flex: 0.25,
+    field: 'P. Kilo',
+    minWidth: 100,
+    headerName: 'P. Kilo',
+    renderCell: ({ row }) => <Typography>{row.name}</Typography>
+  },
+  {
+    flex: 0.25,
+    field: 'P. Unitario',
+    minWidth: 100,
+    headerName: 'P. Unitario',
+    renderCell: ({ row }) => <Typography>{row.name}</Typography>
+  },
+  {
+    flex: 0.25,
+    field: 'Categoría',
+    minWidth: 150,
+    headerName: 'Categoría',
+    renderCell: ({ row }) => <Typography>{row.name}</Typography>
+  },
+  {
+    flex: 0.25,
+    minWidth: 215,
+    field: 'createdDate',
+    headerName: 'Marca',
+    renderCell: ({ row }) => <Typography variant='body2'>{row.createdDate}</Typography>
+  }
+]
+
+const PermissionsTable = () => {
+  // ** State
+  const [value, setValue] = useState('')
+  const [pageSize, setPageSize] = useState(10)
+  const [editValue, setEditValue] = useState('')
+  const [editDialogOpen, setEditDialogOpen] = useState(false)
+
+  // ** Hooks
+
+  const dispatch = useDispatch()
+  const store = useSelector(state => state.permissions)
+  useEffect(() => {
+    dispatch(
+      fetchData({
+        q: value
+      })
+    )
+  }, [dispatch, value])
+
+  const handleFilter = useCallback(val => {
+    setValue(val)
+  }, [])
+
+  const handleEditPermission = name => {
+    setEditValue(name)
+    setEditDialogOpen(true)
+  }
+  const handleDialogToggle = () => setEditDialogOpen(!editDialogOpen)
+
+  const onSubmit = e => {
+    setEditDialogOpen(false)
+    e.preventDefault()
+  }
+
+  const columns = [
+    ...defaultColumns,
+    {
+      flex: 0.15,
+      minWidth: 115,
+      sortable: false,
+      field: 'actions',
+      headerName: 'Acciones',
+      renderCell: ({ row }) => (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton onClick={() => handleEditPermission(row.name)}>
+            <Icon icon='mdi:pencil-outline' />
+          </IconButton>
+          <IconButton>
+            <Icon icon='mdi:delete-outline' />
+          </IconButton>
+        </Box>
+      )
+    }
+  ]
+
   return (
-    <Grid container spacing={6}>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title='Create Awesome 🙌'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>PRODUCTOS.</Typography>
-            <Typography>
-              Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin. Carrot cake dragée chupa chups jujubes.
-              Macaroon liquorice cookie wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
-            </Typography>
-          </CardContent>
-        </Card>
+    <>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <PageHeader
+            title={<Typography variant='h5'>Productos Mundo Animal</Typography>}
+            subtitle={<Typography variant='body2'>Tienda de Accesorios y Alimentos de Mascotas</Typography>}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <TableHeader value={value} handleFilter={handleFilter} />
+            <DataGrid
+              autoHeight
+              rows={store.data}
+              columns={columns}
+              pageSize={pageSize}
+              disableSelectionOnClick
+              rowsPerPageOptions={[10, 25, 50]}
+              onPageSizeChange={newPageSize => setPageSize(newPageSize)}
+              sx={{ '& .MuiDataGrid-columnHeaders': { borderRadius: 0 } }}
+            />
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+      <Dialog maxWidth='sm' fullWidth onClose={handleDialogToggle} open={editDialogOpen}>
+        <DialogTitle sx={{ mx: 'auto', textAlign: 'center' }}>
+          <Typography variant='h5' component='span' sx={{ mb: 2 }}>
+            Edit Permission
+          </Typography>
+          <Typography variant='body2'>Edit permission as per your requirements.</Typography>
+        </DialogTitle>
+        <DialogContent sx={{ mx: 'auto' }}>
+          <Alert severity='warning' sx={{ maxWidth: '500px' }}>
+            <AlertTitle>Warning!</AlertTitle>
+            By editing the permission name, you might break the system permissions functionality. Please ensure you're
+            absolutely certain before proceeding.
+          </Alert>
+
+          <Box component='form' sx={{ mt: 8 }} onSubmit={onSubmit}>
+            <FormGroup sx={{ mb: 2, alignItems: 'center', flexDirection: 'row', flexWrap: ['wrap', 'nowrap'] }}>
+              <TextField
+                fullWidth
+                size='small'
+                value={editValue}
+                label='Permission Name'
+                sx={{ mr: [0, 4], mb: [3, 0] }}
+                placeholder='Enter Permission Name'
+                onChange={e => setEditValue(e.target.value)}
+              />
+
+              <Button type='submit' variant='contained'>
+                Update
+              </Button>
+            </FormGroup>
+            <FormControlLabel control={<Checkbox />} label='Set as core permission' />
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
-ProductsIndex.acl = {
-  action: 'read',
-  subject: 'inventory'
-}
 
-export default ProductsIndex
+export default PermissionsTable
